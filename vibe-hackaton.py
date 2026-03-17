5f83307f897977d45cec1d83c5924187acf3830d
clbokea <clbo@kea.dk>
2026-03-17 17:57:54 +0100

    added vibe-hack groups file for PR

---
+++ vibe-hackaton.py
@@ -0,0 +1,12 @@
+GROUP_REPOS = [
+    {
+        "name": "",
+        "gitLinks": [""],
+        "backend": "",
+        "frontend": "",
+        "monitoring": "",
+        "stack": [],
+        "documentation": [],
+        "members": [],
+    },
+]
